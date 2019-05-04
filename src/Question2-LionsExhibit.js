84f831c2f5e30fc8bca977d1ba7f31e05880d8fe
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-05-04 16:31:51 +0100

    Add another bit of text to make it more realistic

--- src/Question2-LionsExhibit.js
+++ src/Question2-LionsExhibit.js
@@ -4,7 +4,8 @@
  * Create a new component named LionsExhibit.
  *
  * It should render an article element with a class "lions". Within the article,
- * render an h2 element with the text "Lions Exhibit".
+ * render an h2 element with the text "Lions". Below the h2, render a p element
+ * with the text "The mane is the most recognisable feature of the species"
  */
 
 import React from "react";
--- src/__tests__/Question2-LionsExhibit.test.js
+++ src/__tests__/Question2-LionsExhibit.test.js
@@ -11,6 +11,13 @@ describe("Question 2 - LionsExhibit", () => {
 
   it("renders a heading with appropriate text", () => {
     const component = mount(<LionsExhibit />);
-    expect(component.find(".lions h2").text()).toBe("Lions Exhibit");
+    expect(component.find(".lions h2").text()).toBe("Lions");
+  });
+
+  it("renders a paragraphs with the appropriate text", () => {
+    const component = mount(<LionsExhibit />);
+    expect(component.find(".lions p").text()).toBe(
+      "The mane is the most recognisable feature of the species"
+    );
   });
 });
